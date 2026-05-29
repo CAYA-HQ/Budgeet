a960e782c790d41e6a7699166453ef6297f089a7
yungemmy892-maker <yungemmy892@gmail.com>
2026-05-29 06:45:45 +0100

    Added a profile folder where picture and other settings are being accepted

--- backend/backend/budgeet/settings.py
+++ backend/backend/budgeet/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     "apps.auth_app",
     "apps.finance",
     "apps.notifications",
+    "apps.profile",
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "Budgeet <noreply@budgeet.app>")
+
+# At the bottom
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
